cbb68b312c45250e1b725f4ac06e8ab36fd645e6
SebastianHernandez69 <sh3641046@gmail.com>
2025-02-16 22:30:11 -0600

    Add projects pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from "astro:content";
 const projects = defineCollection({
     schema: z.object({
         nombre: z.string(),
-        img: z.string(),
+        img: z.string().optional(),
         descripcion: z.string(),
         tools: z.array(z.string())
     })
---
+++ src/interfaces/interface.ts
@@ -0,0 +1,7 @@
+export interface ProjectProps {
+    nombre: string,
+    img?: string,
+    descripcion: string
+    tools: string[],
+    href: string
+}
